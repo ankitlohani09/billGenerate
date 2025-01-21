28bf1bb6504cbf8cb0bb7f272bf796427ea578e9
ankitlohani09 <alohani.evision@gmail.com>
2025-01-21 13:34:35 +0530

    AWS Configuration - update

--- src/main/java/com/example/QuotationApplication.java
+++ src/main/java/com/example/QuotationApplication.java
@@ -2,12 +2,18 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class QuotationApplication {
+public class QuotationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuotationApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(QuotationApplication.class);
+	}
 }
